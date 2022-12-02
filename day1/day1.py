5d7ef8f287a6b50889c1f6aa0b09b668f96a79ac
Joni Vrapi <jonivrapi@yahoo.com>
2022-12-02 17:20:03 -0500

    solves day 1 problem

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+path = '/Users/jonivrapi/Documents/GitHub/advent-of-code-2022/day1/input.txt';
+
+maxTotal = 0
+
+with open(path) as input:
+    contents = input.readlines()
+    
+    tempTotal = 0
+
+    for line in contents:
+        if (line == '\n'):
+            if (maxTotal < tempTotal):
+                maxTotal = tempTotal
+            
+            tempTotal = 0
+        else:
+            tempTotal += int(line)
+            print(f'tempTotal: {tempTotal}')
+
+    
+    print(f'Max Total: {maxTotal}')
